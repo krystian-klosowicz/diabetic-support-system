4d5ae5dfc32e06a5bbfa3c27d9ed757a7b9c57cb
Krystian Kłosowicz <krystian.klosowicz7@gmail.com>
2023-09-14 12:35:28 +0200

    Start of the diabetic support system project

---
+++ src/main/java/com/klosowicz/diabetic/support/system/InformationTechnologySystemForSupportingDiabeticsApplication.java
@@ -0,0 +1,13 @@
+package com.klosowicz.diabetic.support.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationTechnologySystemForSupportingDiabeticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationTechnologySystemForSupportingDiabeticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klosowicz/diabetic/support/system/InformationTechnologySystemForSupportingDiabeticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klosowicz.diabetic.support.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationTechnologySystemForSupportingDiabeticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
