307341917e8d0d4c5f4faeb7692993a1c79772ef
Krystian Kłosowicz <krystian.klosowicz7@gmail.com>
2023-09-20 15:02:40 +0200

    add BloodPressure entity

---
+++ src/main/java/com/klosowicz/diabetic/support/system/entities/BloodPressureMeasurements.java
@@ -0,0 +1,31 @@
+package com.klosowicz.diabetic.support.system.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class BloodPressureMeasurements {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int sys_pressure;
+
+    private int dia_pressure;
+
+    private int pulse;
+
+    private LocalDate measurement_date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private ApplicationUser user;
+}
