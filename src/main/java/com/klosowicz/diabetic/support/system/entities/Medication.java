d93e4800956dd1039a0d4664d9b0999c6f9fe9d7
Krystian Kłosowicz <krystian.klosowicz7@gmail.com>
2023-09-20 15:13:02 +0200

    add medication entity

---
+++ src/main/java/com/klosowicz/diabetic/support/system/entities/Medication.java
@@ -0,0 +1,29 @@
+package com.klosowicz.diabetic.support.system.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Medication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private double dosage;
+
+    private int freq_per_day;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private ApplicationUser user;
+
+}
