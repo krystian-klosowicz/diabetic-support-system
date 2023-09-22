b9bf509f8b9a4ac79514d9b425392387c49b3148
Krystian Kłosowicz <krystian.klosowicz7@gmail.com>
2023-09-22 15:20:27 +0200

    add PasswordEncoder Beam

--- src/main/java/com/klosowicz/diabetic/support/system/DiabeticSupportSystemApplication.java
+++ src/main/java/com/klosowicz/diabetic/support/system/DiabeticSupportSystemApplication.java
@@ -2,12 +2,22 @@ package com.klosowicz.diabetic.support.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DiabeticSupportSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DiabeticSupportSystemApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+
+		return new BCryptPasswordEncoder();
 	}
 
 }
