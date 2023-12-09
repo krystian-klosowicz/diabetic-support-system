3abcca8ee618dc932d8e13b3f69ad7f758974948
Krystian Kłosowicz <krystian.klosowicz7@gmail.com>
2023-12-09 13:00:12 +0100

    added role claim to jwtToken

--- src/main/java/com/klosowicz/diabetic/support/system/config/JwtService.java
+++ src/main/java/com/klosowicz/diabetic/support/system/config/JwtService.java
@@ -23,86 +23,81 @@ import java.util.function.Function;
 @RequiredArgsConstructor
 public class JwtService {
 
-    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
-    private final UserRepository applicationUserRepository;
+  private static final String SECRET_KEY =
+      "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+  private final UserRepository applicationUserRepository;
 
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
+  public String extractUsername(String token) {
+    return extractClaim(token, Claims::getSubject);
+  }
+
+  public Long extractUserId(String token) {
+    Claims claims = extractAllClaims(token);
+    Object userIdObject = claims.get("userId");
+    Long userId = null;
+    if (userIdObject instanceof Long) {
+      userId = (long) userIdObject;
     }
 
-    public Long extractUserId(String token) {
-        Claims claims = extractAllClaims(token);
-        Object userIdObject = claims.get("userId");
-        Long userId = null;
-        if (userIdObject instanceof Long) {
-            userId = (long) userIdObject;
-        }
-
-        if (userIdObject instanceof Integer) {
-            userId = ((Integer) userIdObject).longValue();
-        }
-
-        return userId;
+    if (userIdObject instanceof Integer) {
+      userId = ((Integer) userIdObject).longValue();
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
+    return userId;
+  }
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+    User user = applicationUserRepository.findByEmail(userDetails.getUsername()).orElseThrow();
+    extraClaims.put("userId", user.getId());
+    extraClaims.put("role", user.getRole());
+
+    return Jwts.builder()
+        .setClaims(extraClaims)
+        .setSubject(userDetails.getUsername())
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+        .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+        .compact();
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
+  private Claims extractAllClaims(String token) {
+    try {
+      return Jwts.parserBuilder()
+          .setSigningKey(getSignInKey())
+          .build()
+          .parseClaimsJws(token)
+          .getBody();
+    } catch (ExpiredJwtException e) { // catching expiredJwtException
+      String subject = token;
+      System.out.println("Error: " + subject + " jwt token failed validation");
+      return new DefaultClaims();
     }
+  }
 
-    public String generateToken(
-            Map<String, Object> extraClaims,
-            UserDetails userDetails
-    ) {
-        User user = applicationUserRepository.findByEmail(userDetails.getUsername()).orElseThrow();
-        extraClaims.put("userId", user.getId());
-
-        return Jwts
-                .builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
-    }
-
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
-    }
-
-    private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    private Claims extractAllClaims(String token) {
-        try {
-            return Jwts
-                    .parserBuilder()
-                    .setSigningKey(getSignInKey())
-                    .build()
-                    .parseClaimsJws(token)
-                    .getBody();
-        } catch (ExpiredJwtException e) { // catching expiredJwtException
-            String subject = token;
-            System.out.println("Error: " + subject + " jwt token failed validation");
-            return new DefaultClaims();
-        }
-
-    }
-
-    private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-
+  private Key getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
 }
